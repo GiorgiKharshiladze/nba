16ddeb1dfa0b737af57ec7be6593ca01cb8f48b2
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-06-13 20:56:34 -0400

    routing works

--- index.php
+++ index.php
@@ -1,51 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+require 'AltoRouter.php';
+$router = new AltoRouter();
+// ROUTES
+$router->map('GET', '/', 'main.php', 'main');
+$router->map('GET', '/add', 'add.php');
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>NBA</title>
-  </head>
-  <body>
-    
-	
-	<table class="table">
-	  <thead class="thead-dark">
-	    <tr>
-	      <th scope="col">#</th>
-	      <th scope="col">Game</th>
-	      <th scope="col">URL</th>
-	      <th scope="col">Datetime</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	  	<?php 
-
-	  	?>
-	    <tr>
-	      <th scope="row">1</th>
-	      <td>Mark</td>
-	      <td><button type="button" class="btn btn-success btn-sm btn-block">Block level button</button></td>
-	      <td>@mdo</td>
-	    </tr>
-	    <tr>
-	      <th scope="row">1</th>
-	      <td>Mark</td>
-	      <td><button type="button" class="btn btn-success btn-sm btn-block">Block level button</button></td>
-	      <td>@mdo</td>
-	    </tr>
-	  </tbody>
-	</table>
-	
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+// OPEN ROUTE OR 404
+$match = $router->match();
+if($match) {
+  require $match['target'];
+} else {
+  header("HTTP/1.0 404 Not Found");
+  require '404.php';
+}
